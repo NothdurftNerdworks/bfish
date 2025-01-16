f1e18e3802e0ee899148dd844b74db097c80670c
Ralph Nothdurft <150295967+NothdurftNerdworks@users.noreply.github.com>
2025-01-15 18:54:17 -0600

    Update BFishClassTest.m

--- BFishClassTest.m
+++ BFishClassTest.m
@@ -15,41 +15,49 @@ classdef BFishClassTest < matlab.unittest.TestCase
     methods (Test)
         % Test methods
         function changelanguage(testCase)
+            error("not implemented")
 
         end
 
         function translatewords(testCase)
-            % Test code
+            error("not implemented")
+
         end
 
         function preservepadding(testCase)
+            error("not implemented")
 
         end
 
         function matchcase(testCase)
+            error("not implemented")
 
         end
 
         function addword(testCase)
+            error("not implemented")
 
         end
 
         function addlanguage(testCase)
+            error("not implemented")
 
         end
 
         function silenterrorevent(testCase)
+            error("not implemented")
 
         end
 
         function savelibrary(testCase)
+            error("not implemented")
 
         end
 
         function loadlibrary(testCase)
+            error("not implemented")
 
         end
-
 
         function unimplementedTest(testCase)
             testCase.verifyFail("Unimplemented test");
